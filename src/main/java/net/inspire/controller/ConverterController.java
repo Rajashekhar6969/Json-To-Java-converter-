dce1c445d79c47cff3e9e7e1758260783b1b7665
rajashekar.arroju <rajashekar.arroju@localhost>
2022-01-31 14:07:31 +0530

    commit

--- src/main/java/net/inspire/controller/ConverterController.java
+++ src/main/java/net/inspire/controller/ConverterController.java
@@ -16,8 +16,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.inspire.pojo.TLesson;
 import net.inspire.pojo.TigerCourse;
+import net.inspire.pojo.Tpage;
 import net.inspire.pojo.vo.CourseVO;
 import net.inspire.pojo.vo.LessonVO;
+import net.inspire.pojo.vo.PageVO;
 
 @RestController
 public class ConverterController {
@@ -29,11 +31,13 @@ public class ConverterController {
 		try {
 			CourseVO courseVo = new CourseVO();
 			ArrayList<LessonVO> lessonList = new ArrayList<LessonVO>();
+			ArrayList<PageVO> pageList = new ArrayList<PageVO>();
 			ObjectMapper om = new ObjectMapper();
 			tigerCourse = om.readValue(new File("src/main/resources/tiger_sample_course/shared/json/structure.json"),
 					TigerCourse.class);
 			System.out.println(tigerCourse.getId());
 			System.out.println(tigerCourse.getLessons().get(0).id);
+			tigerCourse.getLessons().get(0).getPage().get(0).getSrc();
 
 			Object obj = new JSONParser()
 					.parse(new FileReader("src/main/resources/tiger_sample_course/enUS/json/titles.json"));
@@ -45,13 +49,33 @@ public class ConverterController {
 			List<TLesson> lessonlist = tigerCourse.getLessons();
 			for (TLesson lesson : lessonlist) {
 				JSONObject lessonTitles = (JSONObject) jo.get(lesson.getId());
+				//JSONObject pages = (JSONObject) jo.get(lesson.getPage());
 				LessonVO lessonVO = new LessonVO();
 				System.out.println(lessonTitles.get("title"));
 				lessonVO.setLessonTitle(lessonTitles.get("title").toString());
 				lessonVO.setDuration(lessonTitles.get("duration").toString());
+				for(Tpage page:lesson.getPage()) {
+					System.out.println("TEMPLATE----"+page.getTemplate());
+					System.out.println("Source JSON----"+page.getSrc());
+					PageVO pageVO=new PageVO();
+					pageVO.setSrc(page.getSrc());
+					pageVO.setProfile(page.getProfile());
+					pageVO.setTemplate(page.getTemplate());
+					pageVO.setType(page.getType());
+					pageList.add(pageVO);
+				}
+				lessonVO.setPageVO(pageList);
 				lessonList.add(lessonVO);
+				
 			}
+			
+			
+			
+			
 			courseVo.setLessonVO(lessonList);
+			
+			
+			
 			return courseVo.toString();
 
 		} catch (Exception e) {
